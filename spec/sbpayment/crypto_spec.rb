d9cdef73588fea33b5002e6edd8a6e341c45094d
Kohei Hasegawa <ameutau@gmail.com>
2015-09-07 16:25:04 +0900

    Check cipher keys

--- spec/sbpayment/crypto_spec.rb
+++ spec/sbpayment/crypto_spec.rb
@@ -5,23 +5,38 @@ describe Sbpayment::Crypto do
   let(:key)  { SecureRandom.hex 12 }
   let(:iv)   { SecureRandom.hex  8 }
 
-  describe 'encrypt' do
+  context 'when cipher_code and cipher_iv are not defined' do
     let(:data) { 'abcd' }
 
-    it 'can encrypt data' do
-      expect(Sbpayment::Crypto.decrypt(Sbpayment::Crypto.encrypt(data, key: key, iv: iv), key: key, iv: iv)).to eq 'abcd'
+    it 'raises an error' do
+      expect {
+        Sbpayment::Crypto.encrypt(data)
+      }.to raise_error ArgumentError
     end
   end
 
-  describe 'decrypt' do
-    let(:data) { Sbpayment::Crypto.encrypt('abcd', key: key, iv: iv) }
-
-    it 'can decrypt encrypted data' do
-      expect(Sbpayment::Crypto.decrypt(data, key: key, iv: iv)).to eq 'abcd'
+  context 'when cipher_code and cipher_iv are defined' do
+    before do
+      Sbpayment.configure do |x|
+        x.cipher_code = key
+        x.cipher_iv = iv
+      end
     end
 
-    it 'cannot decrypt encrypted data' do
-      expect(Sbpayment::Crypto.decrypt(data, key: SecureRandom.hex(12), iv: SecureRandom.hex(8))).to_not eq 'abcd'
+    describe 'encrypt' do
+      let(:data) { 'abcd' }
+
+      it 'can encrypt data' do
+        expect(Sbpayment::Crypto.decrypt(Sbpayment::Crypto.encrypt(data))).to eq 'abcd'
+      end
+    end
+
+    describe 'decrypt' do
+      let(:data) { Sbpayment::Crypto.encrypt('abcd') }
+
+      it 'can decrypt encrypted data' do
+        expect(Sbpayment::Crypto.decrypt(data)).to eq 'abcd'
+      end
     end
   end
 end
