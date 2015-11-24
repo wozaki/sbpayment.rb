62792e6618b012f53028419413154545509d1664
Kohei Hasegawa <ameutau@gmail.com>
2015-11-24 18:23:01 +0900

    Remove sps transaction id

--- lib/sbpayment/api/softbank/commit.rb
+++ lib/sbpayment/api/softbank/commit.rb
@@ -8,7 +8,6 @@ module Sbpayment
         tag 'sps-api-request', id: 'ST02-00201-405'
         key :merchant_id, default: -> { Sbpayment.config.merchant_id }
         key :service_id,  default: -> { Sbpayment.config.default_service_id }
-        key :sps_transaction_id
         key :tracking_id
         key :request_date, default: -> { Time.now.strftime('%Y%m%d%H%M%S') }
         key :limit_second
