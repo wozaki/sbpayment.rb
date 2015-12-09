074ca154057803cbe364e20216124be3629cb74a
Kenichi Kamiya <kachick1@gmail.com>
2015-12-09 20:44:45 +0900

    Fix a spec typo

--- spec/sbpayment/link/purchase_report_spec.rb
+++ spec/sbpayment/link/purchase_report_spec.rb
@@ -17,7 +17,7 @@ RSpec.shared_context "prepare purchase report params" do
   end
 end
 
-describe Sbpayment::Link::PurchaseResult do
+describe Sbpayment::Link::PurchaseReport do
   context 'when valid params are given' do
     include_context "prepare purchase report params" do
       let!(:query) { 'res_payinfo_key=&service_id=001&auto_charge_type=0&free1=&cust_code=498b1fd98ebeb074ff1d50f2e2720a1a&div_settele=0&free3=&res_sps_payment_no=&service_type=0&free2=&sps_hashcode=BE33A23EC4B61A34545E0A485D0897E33383F4DE&order_id=9eb2fced282c5bfc67c2417217ca2682&camp_type=&res_date=20150807161021&amount=980&res_err_code=&last_charge_month=&limit_second=600&pay_type=2&res_payment_date=20150807161021&merchant_id=19788&tracking_id=&res_tracking_id=80312789324671&request_date=20150807161004&pay_item_id=&res_pay_method=&terminal_type=0&res_sps_cust_no=&pay_method=&tax=&item_name=%8Cp%91%B1%89%DB%8B%E0&sps_payment_no=&res_result=OK&item_id=Item+ID&sps_cust_no=' }
