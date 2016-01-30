be0a9be37796031305ac929302ff804d1b0e37d0
Kohei Hasegawa <ameutau@gmail.com>
2016-01-31 01:02:47 +0900

    Add Sbpayment::Link::RoutineEntry
    The name routine entry is taken from sbps specification.
    This is the endpoint for D01-1

---
+++ lib/sbpayment/link/routine_entry.rb
@@ -0,0 +1,36 @@
+require_relative '../parameter_definition'
+
+module Sbpayment
+  module Link
+    # D01-1
+    class RoutineEntry
+      include ParameterDefinition
+
+      key :pay_method
+      key :merchant_id, default: -> { Sbpayment.config.merchant_id }
+      key :service_id,  default: -> { Sbpayment.config.default_service_id }
+      key :cust_code
+      key :order_id
+      key :item_id
+      key :item_name, type: :M
+      key :service_type
+      key :tracking_id
+      key :terminal_type
+      key :success_url
+      key :cancel_url
+      key :error_url
+      key :pagecon_url
+      key :free1, type: :M
+      key :free2, type: :M
+      key :free3, type: :M
+      key :free_csv, type: :M
+      key :request_date, default: -> { TimeUtil.format_current_time }
+      key :limit_second
+      key :sps_hashcode
+
+      def generate_sps_hashcode(encoding: 'UTF-8')
+        super
+      end
+    end
+  end
+end
