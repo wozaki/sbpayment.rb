570b03c810cffe3ab70bf5afd092f9583f729901
Kohei Hasegawa <ameutau@gmail.com>
2016-02-22 15:58:36 +0900

    Remove unused key
    refs: https://github.com/quipper/sbpayment.rb/pull/35#discussion_r53590658

--- lib/sbpayment/api/softbank/commit.rb
+++ lib/sbpayment/api/softbank/commit.rb
@@ -8,7 +8,6 @@ module Sbpayment
         tag 'sps-api-request', id: 'ST02-00201-405'
         key :merchant_id, default: -> { Sbpayment.config.merchant_id }
         key :service_id,  default: -> { Sbpayment.config.service_id }
-        key :sps_transaction_id
         key :tracking_id
         key :request_date, default: -> { TimeUtil.format_current_time }
         key :limit_second
