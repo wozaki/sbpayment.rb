7a4d8c75a74440116d56fc1c4b19a31daf23ec63
Kenichi Kamiya <kachick1@gmail.com>
2018-08-02 13:14:39 +0900

    Add a filing spec

--- spec/sbpayment/errors_spec.rb
+++ spec/sbpayment/errors_spec.rb
@@ -83,6 +83,18 @@ describe Sbpayment::APIError do
         end
       end
     end
+
+    context 'with a known pattern "40203200"' do
+      subject { Sbpayment::APIError.parse '40203200' }
+
+      it 'returns a API40203Error' do
+        expect(subject).to be_an_instance_of(Sbpayment::API40203Error)
+      end
+
+      pending 'knows the item detail' do
+        expect(subject.item.summary).to eq('取消対象年月')
+      end
+    end
   end
 
   describe '#payment_method' do
